eb99e2b12a14c2bebe25273159935e500853ef82
Edward <60502285+ethndo@users.noreply.github.com>
2024-04-16 21:11:37 +1000

    feat: Prepared codebase for development

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,18 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const App = () => {
-  // TODO: example api call, please delete this when you implement your own components/calls
-  const [message, setMessage] = useState<string>();
-
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('/hello');
-      const { result } = await response.json();
-      setMessage(result);
-    })();
-  });
-
-  return <p>{message}</p>;
+  return <></>;
 };
 
 export { App };
