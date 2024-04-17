3bbbec81f05f4a4b6c629cdb8741d17bfa387f93
Edward <60502285+ethndo@users.noreply.github.com>
2024-04-17 23:08:09 +1000

    feat: Implemented endpoint for retrieving all tasks and adding a new task

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,68 @@
 import express from 'express';
 import ViteExpress from 'vite-express';
+import pg from 'pg';
 
 const app = express();
 
-// TODO: Example route, please delete this when you implement your own routes
-app.get('/hello', (_, res) => {
-  res.json({ result: 'Hello there!' });
+const { Pool } = pg;
+const pool = new Pool({
+  user: 'edward',
+  host: 'localhost',
+  database: 'checkbox_db',
+  password: '123',
+  port: 5432,
+});
+
+// Function to determine the status of the card
+function calculateStatus(dueDate: Date) {
+  const currentDate = new Date();
+  const dateDifference = dueDate - currentDate;
+  const days = dateDifference / (1000 * 60 * 60 * 24);
+
+  if (dueDate < currentDate) {
+    return 'Overdue';
+  } else if (days <= 7) {
+    return 'Due soon';
+  } else {
+    return 'Not urgent';
+  }
+}
+
+// Routes
+app.get('/tasks', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM tasks ORDER BY created_date DESC');
+    res.json(rows);
+  } catch (error) {
+    console.error('There was an error fetching tasks.', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/add_task', async (req, res) => {
+  const { taskName, taskDescription, taskDueDate } = req.body;
+  // Check if name and due date were provided
+  if (!taskName || !taskDueDate) {
+    return res.status(400).json({ error: 'Name and Due Date are required.'});
+  }
+
+  const createdDate = new Date();
+  const dueDate = new Date(taskDueDate);
+  const status = calculateStatus(dueDate);
+
+  const query = `
+    INSERT INTO tasks (name, description, due_date, created_date, status)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+  `;
+
+  try {
+    const { rows } = await pool.query(query, [taskName, taskDescription, taskDueDate, createdDate, status]);
+    res.status(200).json(rows[0])
+  } catch (error) {
+    console.error('There was an error adding a new task.', error);
+    res.status(500).json({ error: 'Internal server error'});
+  }
 });
 
 ViteExpress.config({
